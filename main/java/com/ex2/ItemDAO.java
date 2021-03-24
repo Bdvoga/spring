3185b9117f34911c46fa04137e2617b5208d3c07
Bdvoga <samib@mail.ru>
2021-03-24 19:56:23 +0200

    Урок 2 - Задача 2

--- main/java/com/ex2/ItemDAO.java
+++ main/java/com/ex2/ItemDAO.java
@@ -81,7 +81,7 @@ public class ItemDAO {
         Item item = session.get(Item.class, id);
         session.close();
 
-        return null;
+        return item;
     }
 
     public SessionFactory createSessionFactory() {
